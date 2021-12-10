fd76bcc0a390a3f282a33949512a2d3eb2ceacbd
hblab-thuongbk <thuongbk@hblab.vn>
2021-12-11 01:38:26 +0700

    create admin page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('users', App\Http\Controllers\UserController::class);
+Route::resource('accounts', App\Http\Controllers\AccountController::class);
