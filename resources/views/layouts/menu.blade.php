5c5b295e03fea0e7137c5fa52605754aeca72a59
hungtran97 <hungtran.nh97@gmail.com>
2021-12-12 01:54:03 +0700

    add dashboard

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,12 @@
 <li class="nav-item">
+    <a href="{{ route('home') }}"
+       class="nav-link {{ Request::is('home') ? 'active' : '' }}">
+       <i class="fas fa-home"></i>
+       <p>Dashboard</p>
+    </a>
     <a href="{{ route('accounts.index') }}"
        class="nav-link {{ Request::is('accounts*') ? 'active' : '' }}">
+        <i class="far fa-user"></i> 
         <p>Accounts</p>
     </a>
 </li>
