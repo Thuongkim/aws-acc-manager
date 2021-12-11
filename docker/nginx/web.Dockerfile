10d54297c4e0204ec327b4917f89867bf8367571
anhldgit <anhld@ows.com.vn>
2021-12-11 23:37:06 +0700

    Revert "add docker composer"

--- docker/nginx/web.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:alpine
-
-RUN mkdir -p /var/www
-WORKDIR /var/www/
--- script/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# $ script/run.sh <your command>
-# EG:
-# $ script/run.sh yarn add lodash
-
-docker run --rm -v $(pwd)/src:/app "$@"
